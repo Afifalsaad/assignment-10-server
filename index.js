ead11eee50cc0df60ab889bbf4956c59f4d6131b
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-11-09 19:02:47 +0600

    get and post data to DB

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// assignment10
+// 5pBnPkwHmyoH7kQD
+const uri =
+  "mongodb+srv://assignment10:5pBnPkwHmyoH7kQD@cluster0.avcddas.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hii World");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("community_cleaning_db");
+    const issuesCollection = db.collection("issuesCollection");
+
+    app.get("/issues", async (req, res) => {
+      const issues = issuesCollection.find().sort({ date: -1 }).limit(4);
+      const result = await issues.toArray();
+      res.send(result);
+    });
+
+    app.post("/issues", async (req, res) => {
+      const newIssue = req.body;
+      const result = await issuesCollection.insertOne(newIssue);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("App is running on port", port);
+});
