9af1aad6983f183bf359aa34534c1cd9c6bdd636
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-11-10 12:25:49 +0600

    get category card details

--- index.js
+++ index.js
@@ -29,10 +29,35 @@ async function run() {
     await client.connect();
     const db = client.db("community_cleaning_db");
     const issuesCollection = db.collection("issuesCollection");
+    const categoryCollection = db.collection("categoryCollections");
 
     app.get("/issues", async (req, res) => {
       const issues = issuesCollection.find().sort({ date: -1 }).limit(4);
       const result = await issues.toArray();
+      res.send(result);
+    });
+
+    app.get("/garbage", async (req, res) => {
+      const query = { category: "Garbage" };
+      const result = await categoryCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/illegalConstruction", async (req, res) => {
+      const query = { category: "Illegal Construction" };
+      const result = await categoryCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/brokenPublicProperty", async (req, res) => {
+      const query = { category: "Broken Public Property" };
+      const result = await categoryCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/roadDamage", async (req, res) => {
+      const query = { category: "Road Damage" };
+      const result = await categoryCollection.findOne(query);
       res.send(result);
     });
 
